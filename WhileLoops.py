eb959e7ec708d913e885d2cc37411e97be925456
SK17TH <sk956@kent.ac.uk>
2024-10-28 03:20:08 +0000

    SK2

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,23 @@
-### Part Two -- your code goes here. 
+
+import random
+
+# Generate a random number between 1 and 100
+secret_number = random.randint(1, 100)
+guess = None
+
+print("Welcome to the Guessing Game!")
+print("I'm thinking of a number between 1 and 100.")
+
+# Loop until the user guesses the correct number
+while guess != secret_number:
+    # Get user's guess
+    guess = int(input("Enter your guess: "))
+    
+    # Check if the guess is too low, too high, or correct
+    if guess < secret_number:
+        print("Too low! Try again.")
+    elif guess > secret_number:
+        print("Too high! Try again.")
+    else:
+        print("Congratulations! You've guessed the correct number!")
+
