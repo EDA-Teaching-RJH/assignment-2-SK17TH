3896976e3d9503c6d1f2c1dbceede41f3eb45b7b
SK17TH <sk956@kent.ac.uk>
2024-10-28 03:11:52 +0000

    SK

--- forLoop.py
+++ forLoop.py
@@ -1,1 +1,6 @@
-### Part One -- your code goes here. 
+# Program to print squares of numbers from 1 to 10
+
+for number in range(1, 11):
+    square = number ** 2
+    print(f"The square of {number} is {square}")
+
